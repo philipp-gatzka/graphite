590f06c6170958998a1969a5250e59f25a8f83c4
philipp-gatzka <phil.gatzka@gmail.com>
2025-12-24 11:34:28 +0100

    #8 add graphite-spring-boot-starter module structure

---
+++ graphite-spring-boot-starter/build.gradle.kts
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("graphite.java-conventions")
+    id("graphite.publish-conventions")
+}
+
+description = "Graphite Spring Boot Starter - Auto-configuration for Spring Boot applications"
+
+dependencies {
+    // BOM imports
+    api(platform(libs.spring.boot.bom))
+    annotationProcessor(platform(libs.spring.boot.bom))
+
+    // Core module
+    api(project(":graphite-core"))
+
+    // Spring Boot
+    implementation(libs.spring.boot.autoconfigure)
+    annotationProcessor(libs.spring.boot.configuration.processor)
+
+    // Optional observability dependencies
+    compileOnly(libs.spring.boot.actuator)
+    compileOnly(libs.spring.boot.actuator.autoconfigure)
+    compileOnly(libs.micrometer.core)
+    compileOnly(libs.micrometer.tracing)
+
+    // Testing
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.bundles.testing)
+    testImplementation(libs.spring.boot.starter.test)
+}
---
+++ graphite-spring-boot-starter/src/main/java/io/github/graphite/spring/package-info.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Graphite Spring Boot Starter for auto-configuration.
+ *
+ * <p>This module provides Spring Boot auto-configuration including:
+ * <ul>
+ *   <li>Automatic GraphiteClient bean creation</li>
+ *   <li>Configuration properties binding</li>
+ *   <li>Micrometer metrics integration</li>
+ *   <li>Micrometer tracing integration</li>
+ *   <li>Actuator health indicator</li>
+ * </ul>
+ *
+ * @see io.github.graphite.spring.autoconfigure.GraphiteAutoConfiguration
+ */
+
+package io.github.graphite.spring;
