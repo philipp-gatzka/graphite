1f495c6f692809f8328a4614765e1b4c60ec7249
philipp-gatzka <phil.gatzka@gmail.com>
2025-12-24 11:34:23 +0100

    #8 add graphite-core module structure

---
+++ graphite-core/build.gradle.kts
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+plugins {
+    id("graphite.java-conventions")
+    id("graphite.publish-conventions")
+}
+
+description = "Graphite Core - Runtime client library for type-safe GraphQL queries"
+
+dependencies {
+    // BOM imports
+    api(platform(libs.jackson.bom))
+
+    // Core dependencies
+    api(libs.jackson.databind)
+    api(libs.jetbrains.annotations)
+
+    // Jackson modules
+    implementation(libs.jackson.core)
+    implementation(libs.jackson.annotations)
+    implementation(libs.jackson.datatype.jsr310)
+
+    // Testing
+    testImplementation(platform(libs.junit.bom))
+    testImplementation(libs.bundles.testing)
+}
---
+++ graphite-core/src/main/java/io/github/graphite/package-info.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * Graphite Core module providing the runtime client library for type-safe GraphQL queries.
+ *
+ * <p>This module contains:
+ * <ul>
+ *   <li>{@code GraphiteClient} - The main client interface for executing GraphQL operations</li>
+ *   <li>{@code GraphiteResponse} - Response wrapper with data, errors, and extensions</li>
+ *   <li>HTTP transport layer using Java 11+ HttpClient</li>
+ *   <li>Retry and rate limiting mechanisms</li>
+ *   <li>Custom scalar support</li>
+ *   <li>Request and response interceptors</li>
+ * </ul>
+ *
+ * @see io.github.graphite.GraphiteClient
+ */
+
+package io.github.graphite;
