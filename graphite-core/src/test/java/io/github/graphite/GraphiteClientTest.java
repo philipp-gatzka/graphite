08c989bd8a9ddc934b4fda92573462e743f36140
philipp-gatzka <phil.gatzka@gmail.com>
2025-12-24 12:43:04 +0100

    #23 add graphiteclient tests

---
+++ graphite-core/src/test/java/io/github/graphite/GraphiteClientTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.graphite;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("GraphiteClient")
+class GraphiteClientTest {
+
+  @Nested
+  @DisplayName("builder")
+  class Builder {
+
+    @Test
+    @DisplayName("should return a builder instance")
+    void shouldReturnBuilderInstance() {
+      var builder = GraphiteClient.builder();
+
+      assertThat(builder).isNotNull();
+      assertThat(builder).isInstanceOf(GraphiteClientBuilder.class);
+    }
+
+    @Test
+    @DisplayName("build should throw UnsupportedOperationException until implemented")
+    void buildShouldThrowUnsupportedOperationException() {
+      var builder = GraphiteClient.builder();
+
+      assertThatThrownBy(builder::build)
+          .isInstanceOf(UnsupportedOperationException.class)
+          .hasMessageContaining("not yet available");
+    }
+  }
+
+  @Nested
+  @DisplayName("interface contract")
+  class InterfaceContract {
+
+    @Test
+    @DisplayName("should extend AutoCloseable")
+    void shouldExtendAutoCloseable() {
+      assertThat(AutoCloseable.class).isAssignableFrom(GraphiteClient.class);
+    }
+  }
+}
