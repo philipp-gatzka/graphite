b5a47fa17de4e5a86854bef0342603a6903f9bb6
philipp-gatzka <phil.gatzka@gmail.com>
2025-12-25 11:38:12 +0100

    #150 simplify lambda exception in longcoercingtest

--- graphite-core/src/test/java/io/github/graphite/scalar/LongCoercingTest.java
+++ graphite-core/src/test/java/io/github/graphite/scalar/LongCoercingTest.java
@@ -97,7 +97,8 @@ class LongCoercingTest {
     @Test
     @DisplayName("should throw for unsupported type")
     void shouldThrowForUnsupportedType() {
-      assertThatThrownBy(() -> coercing.deserialize(new Object()))
+      Object unsupportedType = new Object();
+      assertThatThrownBy(() -> coercing.deserialize(unsupportedType))
           .isInstanceOf(IllegalArgumentException.class)
           .hasMessageContaining("Cannot coerce");
     }
