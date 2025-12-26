901cc646bc02dd5b05448823ae5d8bd20fa41781
philipp-gatzka <phil.gatzka@gmail.com>
2025-12-26 11:44:13 +0100

    #271 add foojay toolchain resolver for auto-provisioning

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,11 @@ pluginManagement {
     }
 }
 
+// Enable automatic JDK provisioning for toolchain-based builds
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.9.0"
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
